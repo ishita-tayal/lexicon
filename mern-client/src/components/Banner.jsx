2874250935972a3cef97be020e0afc18ca33de95
Ishita Tayal <ishitatayal05@gmail.com>
2025-04-11 01:30:00 +0530

    continuing homepage

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Banner = () => {
     return (
         <div className='px-4 lg:px-24 bg-teal-100 flex items-center'>
-            Banner
+            <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40'>
+                {/* left side */}
+                <div>
+                    <h2 className='text-5xl font-bold leading-snug text-black'>Buy and Sell Your Books</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat temporibus aperiam praesentium. Expedita eos atque blanditiis libero, molestias enim laudantium omnis quasi quibusdam inventore cupiditate earum. Sequi quia cum totam.</p>
+                    <div>
+                        <input type='search' name='search' id='search' placeholder='Search a book' />
+                    </div>
+                </div>
+
+                {/* right side */}
+                <div>Right Side</div>
+            </div>
         </div>
     )
 }
