eac3436931fe7ccd2340f35c9a8b0e01da582538
Ishita Tayal <ishitatayal05@gmail.com>
2025-04-11 01:07:10 +0530

    frontend and styling started

---
+++ mern-client/src/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className='h-screen'>
+            <div className='h-screen'>
+                Home
+            </div>
+            <div className='h-screen bg-red'>
+                hello
+            </div>
+        </div>
+    )
+}
+
+export default Home
--- mern-client/src/routers/router.jsx
+++ mern-client/src/routers/router.jsx
@@ -2,10 +2,35 @@ import {
     createBrowserRouter,
     RouterProvider,
 } from "react-router-dom";
+import App from "../App"
+import Home from "../home/Home";
+import Shop from "../shop/Shop";
+import About from "../components/About";
+import Blog from "../components/Blog";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <div>Hello world!</div>,
+        element: <App />,
+        children: [
+            {
+                path: '/',
+                element: <Home />
+            },
+            {
+                path: "/shop",
+                element: <Shop />
+            },
+            {
+                path: "/about",
+                element: <About />
+            },
+            {
+                path: "/blog",
+                element: <Blog />
+            }
+        ]
     },
 ]);
+
+export default router;
