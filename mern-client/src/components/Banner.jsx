6adc3fe11ba24dfc2966b50f0d9f4207ea198c8e
Ishita Tayal <ishitatayal05@gmail.com>
2025-04-11 02:08:04 +0530

    made search box

--- mern-client/src/components/Banner.jsx
+++ mern-client/src/components/Banner.jsx
@@ -9,7 +9,8 @@ const Banner = () => {
                     <h2 className='text-5xl font-bold leading-snug text-black'>Buy and Sell Your Books</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat temporibus aperiam praesentium. Expedita eos atque blanditiis libero, molestias enim laudantium omnis quasi quibusdam inventore cupiditate earum. Sequi quia cum totam.</p>
                     <div>
-                        <input type='search' name='search' id='search' placeholder='Search a book' />
+                        <input type='search' name='search' id='search' placeholder='Search a book' className='py-2 px-2 rounded-s-sm outline-none bg-white' />
+                        <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200'>Search</button>
                     </div>
                 </div>
 
