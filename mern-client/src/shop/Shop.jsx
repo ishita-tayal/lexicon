182a16f0aa472689ebb030cd8637ab46084db2f8
Ishita Tayal <ishitatayal05@gmail.com>
2025-04-14 02:21:23 +0530

    starting shop

--- mern-client/src/shop/Shop.jsx
+++ mern-client/src/shop/Shop.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Shop = () => {
+    const [books, setBooks] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data));
+    }, [])
     return (
         <div>Shop</div>
     )
